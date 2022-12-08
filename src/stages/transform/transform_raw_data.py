aa69c2d79d39591f5e0c2cc6c66474cde2a2d970
Hugo Barbosa <hugopaulino12@gmail.com>
2022-12-08 11:16:48 -0300

    Finalizado a parte de transformacao.

--- src/stages/transform/transform_raw_data.py
+++ src/stages/transform/transform_raw_data.py
@@ -3,39 +3,21 @@
 
 from typing import List, Dict
 from src.stages.contracts.extract_contract import ExtractContract
+from src.stages.contracts.transform_contract import TransformContract
+from src.errors.transform_error import TransformError
 
 class TransformRawData:
 
-    def transform(self, extract_contract: ExtractContract):
-        transformed_information = self.__filter_and_transform_data(extract_contract)
-        return transformed_information
+    def transform(self, extract_contract: ExtractContract) -> TransformContract:
+        try:
+            transformed_information = self.__filter_and_transform_data(extract_contract)
+            transformed_data_contract = TransformContract(
+                load_content=transformed_information
+            )
 
-    def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
-        extraction_date = extract_contract.extraction_date
-        data_content = extract_contract.raw_information_content
-        transformed_information = []
-
-        for data in data_content:
-            transform_data = None
-            link = data["link"]
-
-            if 'artistid=' not in link:
-                continue
-
-            names = None
-
-            if ', ' in data["name"]:
-                names = data["name"].split(', ')
-            elif ' ' in data["name"]:
-                names = data["name"].split(' ')
-            else:
-                names = [data["name"]]
-
-            transform_data = self.__transform_data(names, link)
-            transform_data["extraction_date"] = extraction_date
-            transformed_information.append(transform_data)
-
-        return transformed_information
+            return transformed_data_contract
+        except Exception as exception:
+            raise TransformError(str(exception)) from exception
 
     def __transform_data(self, names: List, link: str) -> Dict:
         link_splited = link.split('artistid=')
@@ -66,3 +48,30 @@ class TransformRawData:
                 "artist_id": link_splited[1],
                 "link": link
             }
+
+    def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
+        extraction_date = extract_contract.extraction_date
+        data_content = extract_contract.raw_information_content
+        transformed_information = []
+
+        for data in data_content:
+            transform_data = None
+            link = data["link"]
+
+            if 'artistid=' not in link:
+                continue
+
+            names = None
+
+            if ', ' in data["name"]:
+                names = data["name"].split(', ')
+            elif ' ' in data["name"]:
+                names = data["name"].split(' ')
+            else:
+                names = [data["name"]]
+
+            transform_data = self.__transform_data(names, link)
+            transform_data["extraction_date"] = extraction_date
+            transformed_information.append(transform_data)
+
+        return transformed_information
