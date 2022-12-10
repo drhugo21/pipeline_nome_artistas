2617eac58cf31e72fb4e905b748334914e311282
Hugo Barbosa <hugopaulino12@gmail.com>
2022-12-09 22:40:48 -0300

    Finalizada a parte de carregar e dos testes, faltando apenas o pipeline em si.

--- src/infra/database_repository.py
+++ src/infra/database_repository.py
@@ -1,10 +1,11 @@
 from typing import Dict
 from .database_connector import DatabaseConnector
+from .interfaces.database_repository import DatabaseRepositoryInterface
 
-class DatabaseRepository:
+class DatabaseRepository(DatabaseRepositoryInterface):
 
     @classmethod
-    def insert_artist(cls, data: Dict) -> None:
+    def insert_artist(cls, data: Dict) -> None: #pylint: disable=arguments-differ
         query = '''
                 INSERT INTO pipeline_db.artists(
                                                 first_name, second_name, surname,
